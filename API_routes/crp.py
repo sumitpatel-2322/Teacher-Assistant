436a7c22a779d80441875cbcca0ba6d545cd7453
Shanta Kompi <shantakompi9@gmail.com>
2026-01-16 11:48:50 +0530

    fixed some bugs

--- API_routes/crp.py
+++ API_routes/crp.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates 
+from API_routes.auth import require_login
 router=APIRouter()
 templates=Jinja2Templates(directory="templates")
 @router.get("/crp")
-def crp(request:Request):
-    return templates.TemplateResponse("CRP.html",{
-        "request":request
+def crp(request:Request,
+        user=Depends(require_login)):
+    return templates.TemplateResponse("CRP.html",
+    {
+        "request":request,
+        "user": user
     })
